392cc252fb8a841c265e972f0e3c4c615066da09
by1459 <by1459@cs.washington.edu>
2022-12-25 18:04:35 -0800

    Added about me and main page. Currently, implementing layout component.

---
+++ src/pages/about.tsx
@@ -0,0 +1,24 @@
+// Step 1: Import React
+import * as React from 'react'
+import {Link} from "gatsby";
+
+// Step 2: Define your component
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <Link to="/">Back home</Link>
+            <p>My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104.
+                To all law enforcement entities, this is not an admission of guilt. I am speaking to my family now.
+                Skyler, you are the love of my life. I hope you know that. Walter Jr., you're my big man. There are
+                going to be some things that you'll come to learn about me in the next few days. But just know that no
+                matter how it may look, I only had you in my heart. Goodbye.
+            </p>
+        </main>
+    )
+}
+
+export const Head = () => <title>About Me</title>
+
+// Step 3: Export your component
+export default AboutPage
